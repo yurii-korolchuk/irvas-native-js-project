61d674290453a344b07392f48ecbf6b43d0d5ce2
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-07-29 00:30:38 +0300

    add fetch api for forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import './slider';
 import modal from './modules/modalWindow';
 import tabs from './modules/tabs';
+import form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
     tabs();
+    form();
 });
---
+++ src/js/modules/form.js
@@ -0,0 +1,53 @@
+const form = () => {
+    const forms = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+
+    const message = {
+        lodiang: 'Загрузка...',
+        success: 'Спасибо! Мы с Вами скоро свяжемся!',
+        error: 'Упс! Что-то пошло не так...'
+    };
+
+    const postFormData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            body: data
+        }) 
+        return await res.text();
+    };
+
+    const clearInputs = (inputs) => {
+        inputs.forEach(item => {
+            item.value = '';
+        })
+    }
+
+    forms.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.lodiang;
+
+            const data = new FormData(item);
+            item.appendChild(statusMessage);
+            postFormData('assets/server.php', data)
+                .then(res => {
+                    statusMessage.textContent = message.success;
+                })
+                .catch(error => {
+                    statusMessage.textContent = message.error;
+                })
+                .finally(() => {
+                    clearInputs(inputs);
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 6000)
+                })
+
+        });
+    });
+};
+
+export default form;
